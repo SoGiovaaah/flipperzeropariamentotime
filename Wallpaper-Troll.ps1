0245674c7598ab0e7fe091d1ebc61a1cf0e6feb5
SoGiovaah <80156722+SoGiovaaah@users.noreply.github.com>
2025-03-25 21:53:29 +0100

    Update Wallpaper-Troll.ps1

--- Wallpaper-Troll.ps1
+++ Wallpaper-Troll.ps1
@@ -25,8 +25,8 @@ $loopJob = Start-Job -ScriptBlock {
     }
 }
 
-$imageUrl = "https://media.italianliferoleplay.it/media/python.png"
-$audioUrl = "https://media.italianliferoleplay.it/media/python.wav"
+$imageUrl = "https://raw.githubusercontent.com/SoGiovaaah/flipperzeropariamentotime/refs/heads/main/python.png"
+$audioUrl = "https://raw.githubusercontent.com/SoGiovaaah/flipperzeropariamentotime/refs/heads/main/python.wav"
 $imagePath = "$env:TEMP\sfondo.png"
 $audioPath = "$env:TEMP\e.wav"
 
